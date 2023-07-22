2599896fa99c4f54b4f9addc1fb0cbda9f3797e7
joshhunt <josh@trtr.co>
2023-07-22 22:24:32 +0100

    bb

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const breakpoints = {
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
+};
+
 export default extendTheme({
+  breakpoints,
   fonts: {
     body: `"Inter",  system-ui, sans-serif`,
     heading: `"Inter", system-ui,  sans-serif`,
--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,27 +1,57 @@
 import { useWorlds } from "../../supabaseClient";
 import { Link } from "wouter";
 import Auth from "../../Auth";
-import { Heading } from "@chakra-ui/react";
+import {
+  Box,
+  SimpleGrid,
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  CardFooter,
+  Stack,
+  Card,
+  CardBody,
+  Heading,
+} from "@chakra-ui/react";
 
 export default function HomeView() {
   const [worlds, error] = useWorlds();
 
   return (
-    <div>
-      <Heading size="2xl">Worlds</Heading>
-      <h2>Worlds</h2>
+    <Box maxW="800px" p={4}>
+      <Heading size="xl">Worlds</Heading>
 
-      {error ? <div>error loading worlds</div> : null}
+      {error ? (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertDescription>Error loading worlds</AlertDescription>
+        </Alert>
+      ) : null}
 
-      <ul>
+      <SimpleGrid columns={2} gap={8}>
         {worlds.map((world) => (
-          <li>
-            <Link href={`/${world.slug}`}> {world.name}</Link>
-          </li>
+          <Card>
+            <CardBody>
+              {/* <Image
+                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              /> */}
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{world.name}</Heading>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <Link href={`/${world.slug}`}>View world</Link>
+              {/* <Button variant="ghost" colorScheme="blue">
+                View world
+              </Button> */}
+            </CardFooter>
+          </Card>
         ))}
-      </ul>
+      </SimpleGrid>
 
       <Auth />
-    </div>
+    </Box>
   );
 }
